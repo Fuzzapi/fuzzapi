5fa6054d37c35c96f6488639dff62a9491b3e1f7
Lalith <lalithr95@gmail.com>
2016-09-09 00:36:40 -0400

    new one

---
+++ config/initializers/redis.rb
@@ -0,0 +1,1 @@
+Redis.current = Redis.new(url: ENV['REDISCLOUD_URL'])
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,26 +1,16 @@
-if Rails.env.production?
+Sidekiq.logger = Rails.logger
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 2 }
-  end
+Sidekiq.default_worker_options = { 'backtrace' => true }
 
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 52 }
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 
-    Rails.application.config.after_initialize do
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      ActiveRecord::Base.connection_pool.disconnect!
+  dbconfig = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+  dbconfig['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+  dbconfig['pool']              = Sidekiq.options[:concurrency] + 2
+  ActiveRecord::Base.establish_connection(dbconfig)
+end
 
-      ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 16
-        ActiveRecord::Base.establish_connection(config)
-
-        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      end
-    end
-  end
-
-end  
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
+end
