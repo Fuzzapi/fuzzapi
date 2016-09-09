cb898e4cac384be5d623be7b3767e9306203a177
Lalith <lalithr95@gmail.com>
2016-09-08 23:09:35 -0400

    use sidekiq calculations

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,21 @@
-require 'sidekiq'
+require 'sidekiq_calculations'
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  sidekiq_calculations = SidekiqCalculations.new
+  sidekiq_calculations.raise_error_for_env!
+
+  config.redis = {
+    url: ENV['REDISCLOUD_URL'],
+    size: sidekiq_calculations.client_redis_size
+  }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 2 }
+  sidekiq_calculations = SidekiqCalculations.new
+  sidekiq_calculations.raise_error_for_env!
+
+  config.options[:concurrency] = sidekiq_calculations.server_concurrency_size
+  config.redis = {
+    url: ENV['REDISCLOUD_URL']
+  }
 end
---
+++ lib/sidekiq_calculations.rb
@@ -0,0 +1,66 @@
+class SidekiqCalculations
+  DEFAULT_CLIENT_REDIS_SIZE  = 2
+  DEFAULT_SERVER_CONCURRENCY = 25
+
+  def raise_error_for_env!
+    return if !Rails.env.production?
+
+    web_dynos
+    worker_dynos
+    max_redis_connection
+  rescue KeyError, TypeError # Integer(nil) raises TypeError
+    raise <<-ERROR
+Sidekiq Server Configuration failed.
+!!!======> Please add ENV:
+  - NUMBER_OF_WEB_DYNOS
+  - NUMBER_OF_WORKER_DYNOS
+  - MAX_REDIS_CONNECTION
+    ERROR
+  end
+
+  def client_redis_size
+    return DEFAULT_CLIENT_REDIS_SIZE if !Rails.env.production?
+
+    puma_workers * (puma_threads/2) * web_dynos
+  end
+
+  def server_concurrency_size
+    return DEFAULT_SERVER_CONCURRENCY if !Rails.env.production?
+
+    (max_redis_connection - client_redis_size - sidekiq_reserved) / worker_dynos / paranoid_divisor
+  end
+
+  private
+    def web_dynos
+      Integer(ENV.fetch('NUMBER_OF_WEB_DYNOS'))
+    end
+
+    def worker_dynos
+      Integer(ENV.fetch('NUMBER_OF_WORKER_DYNOS'))
+    end
+
+    def max_redis_connection
+      Integer(ENV.fetch('MAX_REDIS_CONNECTION'))
+    end
+
+    # ENV used in `config/puma.rb` too.
+    def puma_workers
+      Integer(ENV.fetch("WEB_CONCURRENCY", 2))
+    end
+
+    # ENV used in `config/puma.rb` too.
+    def puma_threads
+      Integer(ENV.fetch("WEB_MAX_THREADS", 5))
+    end
+
+    # https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/redis_connection.rb#L12
+    def sidekiq_reserved
+      5
+    end
+
+    # This is added to bring down the value of Concurrency
+    # so that there's leeway to grow
+    def paranoid_divisor
+      2
+    end
+end
