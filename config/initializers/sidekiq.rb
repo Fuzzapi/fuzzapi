13ab3dc094e5386b36c334b58c3d55105e792234
Lalith <lalithr95@gmail.com>
2016-09-08 22:18:55 -0400

    update sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,19 +6,7 @@ if Rails.env.production?
 
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV['REDIS_URL'], size: 25 }
-
-    Rails.application.config.after_initialize do
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      ActiveRecord::Base.connection_pool.disconnect!
-
-      ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        config['pool'] = 16
-        ActiveRecord::Base.establish_connection(config)
-
-        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      end
-    end
   end
+
+  Sidekiq.default_worker_options = { 'backtrace' => true }
 end
